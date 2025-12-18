6873c2f765c36453f260acfb90268af3579d8b40
Yan Tumar <jantumar.unity@gmail.com>
2025-12-18 19:08:52 +0200

    Implement index view and basic template for home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Cook, Dish, DishType
 
-# Create your views here.
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
