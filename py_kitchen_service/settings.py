0e7a7ddd97bdeedc894edc6b94ab690ea4ea5df9
Yan Tumar <jantumar.unity@gmail.com>
2025-12-22 19:39:58 +0200

    build: configure environment variables for security and update requirements

--- py_kitchen_service/settings.py
+++ py_kitchen_service/settings.py
@@ -1,16 +1,17 @@
+import os
 from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-lhkl&un$e$sm#if+n@3z^%6kbl+j9k(=1y!yj28@jzk6)4k*y#'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",'django-insecure-lhkl&un$e$sm#if+n@3z^%6kbl+j9k(=1y!yj28@jzk6)4k*y#')
 
 
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 INTERNAL_IPS = [
